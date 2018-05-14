ec42856301ec8047ac389673daa1575ec6883cfd
lucascruz96 <lucascruz96@outlook.com>
2018-05-14 20:35:32 -0300

    Projeto Base

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { ProjetosPage } from '../projetos/projetos';
+import { TarefasPage } from '../tarefas/tarefas';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root: any = TarefasPage;
+  tab2Root: any = ProjetosPage;
 
   constructor() {
 
